363f7d981a71bcd41aaadd9c2c012ab31db8e18a
Andre-LSL <andreluizsilvalm@gmail.com>
2024-08-27 23:17:06 -0300

    Adicionando função de IP

--- Exercicio01/src/controller/RedesController.java
+++ Exercicio01/src/controller/RedesController.java
@@ -1,5 +1,5 @@
 package controller;
-
+import java.io.*;
 public class RedesController {
 
     public RedesController() {
@@ -13,18 +13,62 @@ public class RedesController {
     public void ip() {
         String SO = os();
         String cd = "";
-        if (SO == "Windows") {
+        if (SO.contains("Windows")) {
             cd = "ipconfig";
-        } else if (SO == "Linux") {
+            System.err.println(cd);
+        } else if (SO.contains("Linux")) {
             cd = "ifconfig";
+            System.err.println(cd);
         }
-        String[] procDados = 
+        String[] procDados = cd.split(" ");
+        System.err.println(procDados);
         try {
-            Runtime.getRuntime().exec();
+            Process p = Runtime.getRuntime().exec(procDados);
+            InputStream fluxo = p.getInputStream();
+            InputStreamReader leitor = new InputStreamReader(fluxo);
+            BufferedReader buffer = new BufferedReader(leitor);
+            String linha = buffer.readLine();
+            while(linha != null) {
+                if (linha.contains("IPv4")) {
+                    System.out.println(linha);
+                }
+                linha = buffer.readLine();
+            }
+            buffer.close();
+            leitor.close();
+            fluxo.close();
         } catch (Exception e) {
-            
+            System.err.println(e.getMessage());
         }
     }
 
+    public void ping() {
+        String SO = os();
+        String cd = "";
+        if (SO.contains("Windows")) {
+            cd = "ping -4 -n 10 www.google.com.br";
+        } else if (SO.contains("Linux")) {
+            cd = "ping -4 -c 10 www.google.com.br";
+        }
+    String[] procDados = cd.split(" ");
+        try {
+            Process p = Runtime.getRuntime().exec(procDados);
+            InputStream fluxo = p.getInputStream();
+            InputStreamReader leitor = new InputStreamReader(fluxo);
+            BufferedReader buffer = new BufferedReader(leitor);
+            String linha = buffer.readLine();
+            while(linha != null) {
+                if (linha.contains("Média") || linha.contains("avg")) {
+                    System.out.println(linha);
+                }
+                linha = buffer.readLine();
+            }
+            buffer.close();
+            leitor.close();
+            fluxo.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 
 }
--- Exercicio01/src/view/ClasseMain.java
+++ Exercicio01/src/view/ClasseMain.java
@@ -1,5 +1,26 @@
 package view;
+import controller.RedesController;
+import javax.swing.JOptionPane;
+    public class ClasseMain {
+        public static void main (String [] args ) {
+            RedesController mController = new RedesController();
+            int opcao = 0;
 
-public class ClasseMain {
-
+            while (opcao != 9) {
+                opcao = Integer.parseInt(JOptionPane.showInputDialog("Menu \n" 
+                                        + "1 - Configuração do IP \n"
+                                        + "2 - Chamada Ping \n" 
+                                        + "9 - Sair"));
+                switch (opcao) {
+                    case 1:
+                        mController.ip();
+                        break;
+                    case 2:
+                        mController.ping();
+                    break;
+                    default:
+                        break;
+                }
+            }
+        }
 }
